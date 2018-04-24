99fe2f3a355d2eabf883d07604ed9abe5347302b
Boy Maas <boy.maas@gmail.com>
2018-04-24 19:01:47 +0200

    Basic setup and readme

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download and validate the abi and wasm files for all contracts, and validate their hashes against our own newly compiled versions.
+
+# Loop all accounts in mongo and validate their existence in a provided snapshot
+# Validate their balances
+
+# Loop the snapshot and make sure all of them are imported
+
+# Validate all totals from chain, and from file
+
+# Validate that the keys for the system accounts has been burnt
+
+# ...
